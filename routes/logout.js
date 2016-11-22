1d69d6779b5269d6a5033f5eed939c7ae3e01e32
brampijper <brampijper@gmail.com>
2016-11-22 17:51:29 +0100

    Added data validation and the log-out

---
+++ routes/logout.js
@@ -0,0 +1,26 @@
+//require the libraries.
+const express 		= require ('express')
+const sequelize 	= require ('sequelize')
+const bodyParser 	= require ('body-parser')
+const session		= require ('express-session')
+const bcrypt		= require ('bcrypt-nodejs')
+const router  		= express.Router ( )
+
+let db = require(__dirname + '/../modules/database')
+
+//The users logs out
+router.get('/logout', (req, res) => {
+	if(!req.session.email) {
+		res.redirect('/login?message=' + encodeURIComponent('Please log-in first;)'))		
+	}
+	else if(req.session.email) {
+		req.session.destroy( (err) => {
+			if(err) console.log(err)
+			else { 
+				res.redirect('/login?message=' + encodeURIComponent('Successfully logged out.'))
+			}
+		})
+	}
+})
+
+module.exports = router
