b6629db6d174970b517b85325dd4d51eae7bd072
brampijper <brampijper@gmail.com>
2016-11-23 17:30:02 +0100

    Trying to exclude completed goals from all the goals

--- routes/goaloverview.js
+++ routes/goaloverview.js
@@ -11,6 +11,22 @@ let db = require(__dirname + '/../modules/database')
 
 router.get('/goaloverview', (req, res) => {
 	if(req.session.user) {
+		// db.Complete.all({
+		// 	where: {
+		// 		userId: req.session.id
+		// 	}
+		// }).then( (completedGoals) => {
+		// 	db.Goal.all({
+		// 		where: {
+		// 			//goalId !not copmletedGoals.goalId
+		// 			//in completedgoals zitten all goals die de user al heeft gedaan
+		// 			//Deze moeten geexclude worden van de goals.
+		// 			//Hoe dan? 
+		// 		}
+		// 	})
+		// 	console.log(result[0].description)
+
+		// 	})
 		res.render('goaloverview')
 	}
 	else {
