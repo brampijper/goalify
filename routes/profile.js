57f323cabddd1e16d27b99758f456bac78718a93
Selma Dorrestein <selmadorrestein@gmail.com>
2016-11-24 17:23:19 +0100

    added possibility to view completed tasks

--- routes/profile.js
+++ routes/profile.js
@@ -20,10 +20,21 @@ router.get('/profile', (req, res) => {
 		//Otherwise, render the profile page, include data of the current user and include the possibility to show a message
 	} else {
 		console.log('\nThe browser will now display the profile.')
-		res.render('profile', {
-			currentUser: user, 
-			message: message
+
+		db.Complete.findAll({
+			where: {userId: req.session.user.id},
+			order: [['updatedAt', 'DESC']],
+			include: [db.Goal]
+		}).then( (goals) => { 
+			res.render('profile', {
+				completedGoals: goals, 
+				currentUser: user, 
+				message: message
+			})
+
 		})
+
+		
 	}
 })
 
@@ -31,31 +42,35 @@ router.get('/profile', (req, res) => {
 
 //Update profile picture
 router.post('/newpic', (req, res) => {
-	
+	console.log(req)
+	console.log('----------------------------------')
+	console.log(req.body.newpic)
 
-	fs.readFile(req.files.img.path, function (err, data) {
-		var newPath = __dirname + "/uploads/uploadedFileName";
-		fs.writeFile(newPath, data, function (err) {
-			console.log(newPath)
-			console.log(data)
-			db.User.findOne({
-				where: {
-					username: req.session.user.username
-				}
-			}).then( (user) => {
-				user.updateAttributes({
-					bio: req.body.newpic,
-				})
+	// fs.readFile(req.body.newpic, function (err, data) {
+	// 	var newPath = __dirname + "/../static";
+	// 	fs.writeFile(newPath, data, function (err) {
+	// 		console.log(newPath)
+	// 		console.log(data)
+	// 		db.User.findOne({
+	// 			where: {
+	// 				username: req.session.user.username
+	// 			}
+	// 		}).then( (user) => {
+	// 			user.updateAttributes({
+	// 				bio: req.body.newpic,
+	// 			})
 
-				res.redirect('/profile?message=' + encodeURIComponent('Your picture has been changed.'))
-				return
-			});
-		});
+	// 			res.redirect('/profile?message=' + encodeURIComponent('Your picture has been changed.'))
+	// 			return
+	// 		});
+	// 	});
 
 
 
-	})
+	// })
 })
+//at this point, i used https://howtonode.org/really-simple-file-uploads as inspiration
+//bram: look into amazon s3
 
 
 //Update bio
