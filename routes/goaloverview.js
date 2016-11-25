3f0ea0a77469eb8ec9b7d7d07dbcddde2592246a
brampijper <brampijper@gmail.com>
2016-11-25 16:27:49 +0100

    The user can now view all the unfinished goals on Google Maps

--- routes/goaloverview.js
+++ routes/goaloverview.js
@@ -13,7 +13,7 @@ let db = require(__dirname + '/../modules/database')
 let goalArray = []
 
 router.get('/goaloverview', (req, res) => {
-	db.Goal.findAll().then( goals => console.log('Number of goals ' + goals.length) )
+	// db.Goal.findAll().then( goals => console.log('Number of goals ' + goals.length) )
 	if(req.session.user) {
 		db.Goal.findAll({
 			include: [{
@@ -24,26 +24,24 @@ router.get('/goaloverview', (req, res) => {
 		}).then( (goals) => {
 			// res.send(goals)
 			for (var i = 0; i < goals.length; i++) {
-				console.log(goals[i].title + ' has ' + goals[i].completes.length + ' completes')
+				// console.log(goals[i].title + ' has ' + goals[i].completes.length + ' completes')
 
 				if(goals[i].completes.length === 0) {
 					goalArray.push(goals[i])
-				}
-				fs.writeFile (__dirname + '/goal.json', JSON.stringify(goalArray), 'utf-8', function(error) {
-					if(error) throw error
-				})			
+				}	
 			}
+			return goalArray
+		}).then( (unfinishedGoals) => {
+			fs.writeFile (__dirname + '/../static/json/goals.json', JSON.stringify(unfinishedGoals), 'utf-8', function(error) {
+				if(error) throw error
+			})	
 		}).then( () => {
 			res.render('goaloverview')
-				//unfinishedgoal: goalArray
 		})
 	}
 	else {
-		res.redirect('/index'
-)	}
+		res.redirect('/index')	
+	}
 })
 
 module.exports = router
-
-//What data do I need for google maps? 
-	//Goal title
