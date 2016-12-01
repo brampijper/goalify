13f4f38ea3fb69f518f9c8d0af0be17daf33a307
brampijper <brampijper@gmail.com>
2016-12-01 20:47:22 +0100

    Fixed the navbar on mobile devices

--- static/head.js
+++ static/head.js
@@ -2,42 +2,16 @@ $(document).ready(function() {
 	  initialize()
 	  $(".button-collapse").sideNav();
 
-$('.dog').animate({
-    width: '25%'
-}, {
-    duration: 2000
-});
+	$('.dog').animate({
+    	width: '25%'
+	}, {duration: 2000})
 
-$('.cat').animate({
-    width: '75%'
-}, {
-    duration: 2000
-});
-	// $('.container-mainbox').css({ height: $(window).innerHeight() });
-	// $(window).resize(function(){
-	// 	$('.container-mainbox').css({ height: $(window).innerHeight() });
- //  	})
+	$('.cat').animate({
+    	width: '75%'
+	}, {duration: 2000})
 })
 
 function initialize() {
-	// var styles = [
- //  	{
-	//     featureType: "road",
-	//     elementType: "geometry",
-	//     stylers: [
-	//       { lightness: 100 },
-	//       { visibility: "simplified" }
- //    	]
- //  	}
-	// ]
-
-	// map.addStyle({
-	//     styledMapName:"Styled Map",
-	//     styles: styles,
-	//     mapTypeId: "map_style"  
-	// });
-
-	// map.setStyle("map_style");
     
    var map = new GMaps({
 		div: '#indexMap',
@@ -50,12 +24,4 @@ function initialize() {
 		streetViewControl: false,
 		overviewMapControl: false
 	});
-
-	// map.addStyle({
-	// 	styledMapName:"Styled Map",
-	// 	styles: styles,
-	// 	mapTypeId: "map_style"
-	// })
-
-	// map.setSTyle("map_style")
 }
