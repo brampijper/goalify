5b2e899d0e9dd2f09329325e69097a89f1f3120c
brampijper <brampijper@gmail.com>
2016-12-01 14:21:25 +0100

    working on the teambar

--- static/head.js
+++ static/head.js
@@ -1,11 +1,3 @@
-var toggled = true;
-
-$('#toggle').click(function() {
-    if (toggled) {
-        $('.cat').animate({ 'width': '70%' }, 1500);
-        toggled = false;
-    } else {
-        $('.dog').animate({ 'width': '30%' }, 1500);
-        toggled = true;
-    }    
-});
+$(document).ready(function() {
+	console.log('yaaay')
+}
